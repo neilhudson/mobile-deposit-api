88eb970377ecebd071df8774d3cfa5562e58d557
Nigel Harniman <nharniman@cloudbees.com>
2015-06-23 14:44:50 +0100

    Changing app name

--- flow.groovy
+++ flow.groovy
@@ -1,22 +1,26 @@
 def buildVersion = null
 stage 'build'
 node('docker') {
-    docker.withServer('tcp://127.0.0.1:1234'){
+//    docker.withServer('tcp://192.168.7.101:1234'){
             docker.image('kmadel/maven:3.3.3-jdk-8').inside('-v /data:/data') {
                 sh 'rm -rf *'
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], clean: true, doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/cloudbees/mobile-deposit-api.git']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/master']], clean: true, doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'ab2d3ee0-76a0-4da3-a86d-7e2574a861bd', url: 'https://github.com/harniman/mobile-deposit-api.git']]])
                 sh 'git checkout master'
-                sh 'git config user.email "kmadel@mac.com"'
-                sh 'git config user.name "kmadel"'
-                sh 'git remote set-url origin git@github.com:cloudbees/mobile-deposit-api.git'
-                sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo clean package'
+                sh 'git config user.email "nigel@harniman.net"'
+                sh 'git config user.name "nharniman"'
+                sh 'git remote set-url origin git@github.com:harniman/mobile-deposit-api.git'
+                sh "mkdir -p /data/mvn"
+                writeFile file: '/data/mvn/settings.xml', text: "<settings><localRepository>/data/mvn/.m2repo</localRepository></settings>"
+
+                sh 'mvn -s /data/mvn/settings.xml clean package'
 
                 stage 'sonar analysis'
-                sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo sonar:sonar'
+//                sh 'mvn -s /data/mvn/settings.xml sonar:sonar'
 
                 stage 'integration-test'
-                sh 'mvn -s /data/mvn/settings.xml -Dmaven.repo.local=/data/mvn/repo verify'
-                //step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
+//                sh 'mvn -s /data/mvn/settings.xml  verify'
+
+//                step([$class: 'JUnitResultArchiver', testResults: '**/target/surefire-reports/TEST-*.xml'])
 
                 stage 'prepare release'
                 def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
@@ -27,25 +31,33 @@ node('docker') {
                 matcher = null
             }
 
-  docker.withServer('tcp://54.165.201.3:2376', 'slave-docker-us-east-1-tls'){
+            docker.withServer('tcp://192.168.99.101:2376', 'slave-docker-us-east-1-tls'){
 
-        stage 'build docker image'
-        def mobileDepositApiImage
-        dir('target') {
-            mobileDepositApiImage = docker.build "kmadel/mobile-deposit-api:${buildVersion}"
-        }
-
-        stage 'deploy to production'
-        try{
-          sh "docker stop mobile-deposit-api"
-          sh "docker rm mobile-deposit-api"
-        } catch (Exception _) {
-           echo "no container to stop"        
-        }
-        mobileDepositApiImage.run("--name mobile-deposit-api -p 8080:8080")
-
-        stage 'publish docker image'
-        sh 'curl -H "Content-Type: application/json" -X POST -d \'{"push_data": {"pushed_at": 1434386606, "images": null, "pusher": "kmadel"}, "callback_url": "https://registry.hub.docker.com/u/kmadel/mobile-bank-api/hook/21a0ic0dje2ff4hg3f3hbg23b5220454b/", "repository": {"status": "Active", "description": "", "is_trusted": false, "full_description": "", "repo_url": "https://registry.hub.docker.com/u/kmadel/mobile-bank-api/", "owner": "kmadel", "is_official": false, "is_private": false, "name": "mobile-bank-api", "namespace": "kmadel", "star_count": 0, "comment_count": 0, "date_created": 1434385021, "repo_name": "kmadel/mobile-bank-api"}}\' http://webhook:13461862c863d7df39e63435eb17deb9@jenkins-latest.beedemo.net/mobile-team/dockerhub-webhook/notify'
-   }
-  }
+                stage 'build docker image'
+                def mobileDepositApiImage
+                dir('target') {
+                    sh "docker ps -a"
+                    mobileDepositApiImage = docker.build "harniman/mobile-deposit-api:${buildVersion}"
+                }
+    
+                stage 'deploy to production'
+                try{
+                  sh "docker stop mobile-deposit-api"
+                  sh "docker rm mobile-deposit-api"
+                } catch (Exception _) {
+                   echo "no container to stop"        
+                }
+                mobileDepositApiImage.run("--name mobile-deposit-api -p 8080:8080")
+    
+                stage 'publish docker image'
+    //          sh 'curl -H "Content-Type: application/json" -X POST -d \'{"push_data": {"pushed_at": 1434386606, "images": null, "pusher": "harniman"}, "callback_url": "https://registry.hub.docker.com/u/harniman/mobile-bank-api/hook/21a0ic0dje2ff4hg3f3hbg23b5220454b/", "repository": {"status": "Active", "description": "", "is_trusted": false, "full_description": "", "repo_url": "https://registry.hub.docker.com/u/harniman/mobile-bank-api/", "owner": "harniman", "is_official": false, "is_private": false, "name": "mobile-bank-api", "namespace": "harniman", "star_count": 0, "comment_count": 0, "date_created": 1434385021, "repo_name": "harniman/mobile-bank-api"}}\' http://webhook:13461862c863d7df39e63435eb17deb9@jenkins-latest.beedemo.net/mobile-team/dockerhub-webhook/notify'
+    // 
+    
+                
+                docker.withRegistry(credentialsId: 'dockerhub-harniman') {
+                    mobileDepositApiImage.push()
+                }
+        
+            }
+//  }
 }
